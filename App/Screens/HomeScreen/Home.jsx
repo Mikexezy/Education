749a796f226b4a190ee0204124f9b11160d74b54
Mikexezy <michelepernaci65@gmail.com>
2024-04-27 16:33:44 +0200

    Visivamente ai livelli manca il contatore delle percentuali delle parti e poi resta solo la parte del livello in se (video + gioco) e piccola sezione profilo

--- App/Screens/HomeScreen/Home.jsx
+++ App/Screens/HomeScreen/Home.jsx
@@ -1,55 +1,91 @@
-import { View, StyleSheet, StatusBar, ScrollView, Dimensions } from 'react-native'
+import { View, StyleSheet, ScrollView, RefreshControl, Dimensions, StatusBar } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { colors } from '../../../assets/Colors/Color';
 import Header from '../../Components/Header/Header';
 import Level from '../../Components/Level/Level';
-import { auth, db } from '../../../firebaseConfig';
-import { get, ref as dbRef } from 'firebase/database';
+import { auth, db, st } from '../../../firebaseConfig';
+import { get, ref } from 'firebase/database';
+import { ref as storageRef, listAll } from 'firebase/storage';
+import { logEvent } from 'firebase/analytics';
 
-const {height, width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 
 export default function Home() {
-  const [levelCount, setLevelCount] = useState([]);
+  const [levelData, setLevelData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
-    const fetchLevelCount = async () => {
-      try {
-        const userId = auth.currentUser.uid;
-        const userRef = dbRef(db, `users/${userId}`);
-        const snapshot = await get(userRef);
-        if (snapshot.exists()) {
-          const userData = snapshot.val();
-          const levelNames = Object.keys(userData);
-          levelNames.sort((a, b) => {
-            const aNumber = parseInt(a.replace('Lvl', ''));
-            const bNumber = parseInt(b.replace('Lvl', ''));
-            return aNumber - bNumber;
-          });
-          setLevelCount(levelNames);
-        } else {
-          console.log("Nessun dato trovato nel nodo utente.");
-        }
-      } catch (error) {
-        console.error("Si è verificato un errore durante il recupero del conteggio dei livelli:", error);
-      }
-    };
-    fetchLevelCount();
+    fetchLevelData();
   }, []);
 
+  const fetchLevelData = async () => {
+    try {
+      const userId = auth.currentUser.uid;
+      const userRef = ref(db, `users/${userId}`);
+      const snapshot = await get(userRef);
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+        const levels = await Promise.all(Object.entries(userData)
+          .sort((a, b) => {
+            const levelNumA = parseInt(a[0].replace("Lvl", ""));
+            const levelNumB = parseInt(b[0].replace("Lvl", ""));
+            return levelNumA - levelNumB;
+          })
+          .map(async ([title, progress]) => {
+            let progressValue = 0; 
+            if (progress !== null && typeof progress === 'object') {
+              progressValue = Object.values(progress).reduce((acc, curr) => acc + curr, 0);
+              if(progressValue != 0)progressValue /= 2;
+            } else {
+              progressValue = progress || 0; 
+            }
+            let stRef = storageRef(st, title);
+            let elementList = await listAll(stRef);
+            const elementListLength = elementList.prefixes.length;
+            if(elementListLength > 0){
+              stRef = storageRef(st, title+"/"+elementList.prefixes[0].name);
+              elementList = await listAll(stRef);
+              title = elementList.items[0].name;
+            }else{
+              title = elementList.items[0].name;
+            }
+            title = title.replace(/Lvl.+$/, '');
+            title = title.split(/(?=[A-Z])/).join(' ');
+            return { title, progress: progressValue };
+          }));
+        setLevelData(levels);
+      } else {
+        console.log("Nessun dato trovato nel nodo utente.");
+      }
+    } catch (error) {
+      console.error("Si è verificato un errore durante il recupero dei dati dei livelli:", error);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchLevelData();
+    setRefreshing(false);
+  };
+
   return (
-      <View style={styles.container}>
-        <Header showProfileButton={true}/>
-        <View style={styles.lvlSection}>
-          <ScrollView contentContainerStyle={styles.lvlList}>
-            <View style={{height: (0.1*height) + StatusBar.currentHeight}}/>
-            {levelCount.map((levelName, index) => (
-              <Level key={index} progress={0} title={levelName}/>
-            ))}
-          </ScrollView>
-        </View>
+    <View style={styles.container}>
+      <Header showProfileButton={true} />
+      <View style={styles.lvlSection}>
+        <ScrollView
+          contentContainerStyle={styles.lvlList}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} progressViewOffset={(0.13*height) + StatusBar.currentHeight}/>}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={{height: (0.1*height) + StatusBar.currentHeight}}/>
+          {levelData.map((level, index) => (
+            <Level key={index} title={level.title} progress={level.progress} />
+          ))}
+        </ScrollView>
       </View>
-  )
-};
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -57,7 +93,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
   },
   lvlSection: {
-    flex:1,
+    flex: 1,
     backgroundColor: "transparent",
   },
   lvlList: {
